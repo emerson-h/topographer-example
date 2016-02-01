b808fcf5d3307e6a48a874d9bd06f6070eb7c99a
Emerson Huitt <emerson.huitt@scimedsolutions.com>
2016-02-01 00:04:04 -0500

    Ensure capybara tests run successfully

--- spec/features/import_inventory_items_spec.rb
+++ spec/features/import_inventory_items_spec.rb
@@ -10,28 +10,30 @@ feature 'Inventory Item Spreadsheet Import' do
 
     expect {
       within('.import-form') do
-        attach_file('import_file', 'spec/fixtures/valid_inventory_items_import.csv')
+        attach_file('import_file', 'spec/fixtures/valid_inventory_items_import.xlsx')
         click_on('Import')
       end
     }.to change { InventoryItem.count }.by(3)
   end
-  scenario 'user imports inventory items with missing information' do
+  scenario 'user imports inventory items with invalid model information' do
     visit inventory_items_path
     click_on 'Import Inventory Items'
 
+    expect {
     within('.import-form') do
-      attach_file('import_file', 'spec/fixtures/valid_inventory_items_import.csv')
+        attach_file('import_file', 'spec/fixtures/invalid_inventory_items_import.xlsx')
       click_on('Import')
     end
+    }.to change { InventoryItem.count }.by(0)
 
-    page.should have_content /Unable to import inventory items/i
+    page.should have_content /Unable to import/i
   end
   scenario 'user imports an inventory item spreadsheet with invalid headers' do
     visit inventory_items_path
     click_on 'Import Inventory Items'
 
     within('.import-form') do
-      attach_file('import_file', 'spec/fixtures/invalid_inventory_items_import.csv')
+      attach_file('import_file', 'spec/fixtures/invalid_header_inventory_items_import.xlsx')
       click_on('Import')
     end
 
